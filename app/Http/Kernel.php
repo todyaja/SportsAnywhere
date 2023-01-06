d1872a61ded4e7d7a45f946b17e647600a46d937
FadlyHaikal <97393065+FadlyHaikal@users.noreply.github.com>
2023-01-06 18:20:32 +0700

    Added middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'authGuest' => \App\Http\Middleware\AuthenticateGuest::class,
+        'authHost' => \App\Http\Middleware\AuthenticateHost::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
