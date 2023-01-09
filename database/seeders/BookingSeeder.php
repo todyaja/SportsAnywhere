15964cc7adde7ebe16e6e440232a910e1a78daff
Shinnozaki <joanne.lee@binus.ac.id>
2023-01-09 22:59:20 +0700

    seeder done

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -18,6 +18,8 @@ class BookingSeeder extends Seeder
     {
         $items = collect([
 
+            // ----------------------------------------------- USER 1 -------------------------------------------------------------------------
+
                 //ongoing
                 [
                     'area_id' => '1',
@@ -89,27 +91,121 @@ class BookingSeeder extends Seeder
 
 
                 //canceled
-                // [
-                //     'area_id' => '7',
-                //     'guest_id' => 1,
-                //     'start_date' => Carbon::now('GMT+7')->addHour('6'),
-                //     'end_date' => Carbon::now('GMT+7')->addHour('8'),
-                //     'cancelled' => 0
-                // ],
-                // [
-                //     'area_id' => '8',
-                //     'guest_id' => 1,
-                //     'start_date' => Carbon::now('GMT+7')->addHour('6'),
-                //     'end_date' => Carbon::now('GMT+7')->addHour('8'),
-                //     'cancelled' => 0
-                // ],
-                // [
-                //     'area_id' => '9',
-                //     'guest_id' => 1,
-                //     'start_date' => Carbon::now('GMT+7')->addHour('6'),
-                //     'end_date' => Carbon::now('GMT+7')->addHour('8'),
-                //     'cancelled' => 0
-                // ],
+                [
+                    'area_id' => '7',
+                    'guest_id' => 1,
+                    'start_date' => Carbon::yesterday('GMT+7'),
+                    'end_date' => Carbon::yesterday('GMT+7')->addHour('2'),
+                    'cancelled' => 1
+                ],
+                [
+                    'area_id' => '8',
+                    'guest_id' => 1,
+                    'start_date' => Carbon::yesterday('GMT+7')->addHour('3'),
+                    'end_date' => Carbon::yesterday('GMT+7')->addHour('5'),
+                    'cancelled' => 1
+                ],
+                [
+                    'area_id' => '9',
+                    'guest_id' => 1,
+                    'start_date' => Carbon::yesterday('GMT+7')->addHour('6'),
+                    'end_date' => Carbon::yesterday('GMT+7')->addHour('8'),
+                    'cancelled' => 1
+                ],
+
+             // ----------------------------------------------- USER 2 -------------------------------------------------------------------------
+             //ongoing
+             [
+                'area_id' => '10',
+                'guest_id' => 2,
+                'start_date' => Carbon::now('GMT+7')->startOfDay()->addHour('9'),
+                'end_date' => Carbon::now('GMT+7')->startOfDay()->addHour('14'),
+                'cancelled' => 0,
+            ],
+            [
+                'area_id' => '11',
+                'guest_id' => 2,
+                'start_date' => Carbon::now('GMT+7')->startOfDay()->addHour('15'),
+                'end_date' => Carbon::now('GMT+7')->startOfDay()->addHour('18'),
+                'cancelled' => 0
+            ],
+            [
+                'area_id' => '12',
+                'guest_id' => 2,
+                'start_date' => Carbon::now('GMT+7')->startOfDay()->addHour('9'),
+                'end_date' => Carbon::now('GMT+7')->startOfDay()->addHour('18'),
+                'cancelled' => 0
+            ],
+
+            //upcoming
+            [
+                'area_id' => '13',
+                'guest_id' => 2,
+                'start_date' => Carbon::tomorrow('GMT+7')->addHour('9'),
+                'end_date' => Carbon::tomorrow('GMT+7')->addHour('11'),
+                'cancelled' => 0
+            ],
+            [
+                'area_id' => '14',
+                'guest_id' => 2,
+                'start_date' => Carbon::tomorrow('GMT+7')->addHour('12'),
+                'end_date' => Carbon::tomorrow('GMT+7')->addHour('14'),
+                'cancelled' => 0
+            ],
+            [
+                'area_id' => '15',
+                'guest_id' => 2,
+                'start_date' => Carbon::tomorrow('GMT+7')->addHour('15'),
+                'end_date' => Carbon::tomorrow('GMT+7')->addHour('17'),
+                'cancelled' => 0
+            ],
+
+            //completed
+            [
+                'area_id' => '1',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('2'),
+                'cancelled' => 0
+            ],
+            [
+                'area_id' => '2',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7')->addHour('3'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('5'),
+                'cancelled' => 0
+            ],
+            [
+                'area_id' => '3',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7')->addHour('6'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('8'),
+                'cancelled' => 0
+            ],
+
+
+            //canceled
+            [
+                'area_id' => '4',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('2'),
+                'cancelled' => 1
+            ],
+            [
+                'area_id' => '5',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7')->addHour('3'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('5'),
+                'cancelled' => 1
+            ],
+            [
+                'area_id' => '6',
+                'guest_id' => 2,
+                'start_date' => Carbon::yesterday('GMT+7')->addHour('6'),
+                'end_date' => Carbon::yesterday('GMT+7')->addHour('8'),
+                'cancelled' => 1
+            ],
 
             ])->each(function ($item) {
                 Booking::create($item);
