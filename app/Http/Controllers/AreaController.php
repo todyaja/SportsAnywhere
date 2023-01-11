c9cd335ed5832673205f94db68e6847345ec8c8a
Tody Prasetya <tody.prasetya@binus.edu>
2023-01-11 16:32:47 +0700

    fix bugs

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -63,7 +63,7 @@ class AreaController extends Controller
 
 
 
-        $latestAreaId = DB::table('areas')->orderByDesc('id')->select('id')->first()->id;
+        // $latestAreaId = DB::table('areas')->orderByDesc('id')->select('id')->first()->id;
 
         //masukkin thumbnail
         $thumbnail_area = $request->area_thumbnail;
@@ -71,8 +71,7 @@ class AreaController extends Controller
         $thumbnail_area_destination = public_path('/assets/areas_thumbnail');
         $thumbnail_area->move($thumbnail_area_destination, $thumbnail_area_file_name);
 
-        Area::create([
-            'id' => $latestAreaId + 1,
+        $insertedArea = Area::create([
             'created_by' => auth()->user()->id,
             'name' => $request->area_name,
             'area_type' => $request->area_type,
@@ -83,22 +82,23 @@ class AreaController extends Controller
             'status' => 0,
         ]);
 
-
         //masukkin foto-foto ke area_pictures
         if (isset($request->area_pictures)) {
+            $idx = 0;
             foreach ($request->area_pictures as $p) {
                 //masukkin foto ke local file
                 $picture_area = $p;
-                $picture_area_file_name = time() . '.' . $picture_area->getClientOriginalExtension();
+                $picture_area_file_name = $idx++ . time() . '.' . $picture_area->getClientOriginalExtension();
                 $picture_area_destination = public_path('/assets/area_pictures');
                 $picture_area->move($picture_area_destination, $picture_area_file_name);
                 //masukkin ke table area_pictures
                 AreaPicture::create([
-                    'area_id' => $latestAreaId,
+                    'area_id' => $insertedArea->id,
                     'pictures' => $picture_area_file_name,
                 ]);
             };
         }
+        dd("done");
 
         return redirect('/')
             ->with('alert', 'Your sport area has been created successfully!');
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -203,47 +203,6 @@ class BookingController extends Controller
         return redirect('/')->with('alert', 'Thank you for your rating !');
     }
 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $update = Booking::where('booking_id', $id)->update([
